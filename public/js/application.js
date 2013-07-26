e136b496528596efc2b45cc1de37113da7e50617
Mitch Seaman <mitchel.seaman@gmail.com>
2013-07-25 17:01:31 -0700

    completed mvp

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
   $('.awesomeness_teller').on('click', function() {
+  	$('h1').text($(this).data('heading'));
+  });
 
+  $('.skill_teller').on('click', function() {
+  	$('h1').text(window.location.pathname.match(/\w*$/));
+  });
+
+  $('.meal_teller').on('click', function() {
+  	$('h1').text($('#meal').val());
   });
 
   // What other events do you need to bind to make the other pages work?
